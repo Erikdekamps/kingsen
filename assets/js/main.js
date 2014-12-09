916a088278324a827b5a0976fd23f2ab49633d8e
Erikdekamps <erikdekamps@gmail.com>
2014-12-09 21:16:23 +0100

    Code rewrite

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+  Sprite = new Image(143,10800);
+  Sprite.src = "img/sprite.png";
+
+  var random = Math.floor(Math.random() * 2) + 1;
+
+  $(".content").html('' +
+    '<h2 class="title">Online Kingsen</h2>' +
+    '<p class="name">Click on the card to start</p>' +
+    '<p class="sprite deck-' + random + '"></p>' +
+    '<p class="description">Click on the card for a random new card.</p>'
+  );
+
+  $(".content").fadeIn('fast');
+
+  $('.content').on("click", function(){
+    $.get('random.php', function(data){
+      $('.content').html(data).show();
+    });
+  });
+});
--- index.php
+++ index.php
@@ -3,53 +3,18 @@
 <head>
   <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=1.0, minimum-scale=1.0, maximum-scale=1.0">
   <meta name="description" content="An online version of the drinking game called Kingsen">
-  <meta name="google-site-verification" content="iiV8BbQqGT1TgpJBq5sZjCTmiqj3SbC8zVIraPVp2UM" />
+  <meta name="google-site-verification" content="iiV8BbQqGT1TgpJBq5sZjCTmiqj3SbC8zVIraPVp2UM"/>
   <link rel="stylesheet" href="style.css"/>
   <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
   <link rel="icon" href="img/favicon.ico" type="image/x-icon">
-  <script>
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-    ga('create', 'UA-56144415-2', 'auto');
-    ga('send', 'pageview');
-  </script>
-<title>Online Kingsen</title>
+  <title>Online Kingsen</title>
 </head>
-  <body>
-    <div id="container">
-      <div class="content"></div>
-    </div>
-  </body>
-</html>
-<?php
-  $iphone = strpos($_SERVER['HTTP_USER_AGENT'],"iPhone");
-  $android = strpos($_SERVER['HTTP_USER_AGENT'],"Android");
-  $palmpre = strpos($_SERVER['HTTP_USER_AGENT'],"webOS");
-  $berry = strpos($_SERVER['HTTP_USER_AGENT'],"BlackBerry");
-  $ipod = strpos($_SERVER['HTTP_USER_AGENT'],"iPod");
-
-  if ($iphone || $android || $palmpre || $ipod || $berry) {
-    echo '<meta id="testViewport" name="viewport" content="width=device-width; height=device-height initial-scale=1.0; maximum-scale=1.0; user-scalable=0;" />';
-    echo '<style>html{-moz-text-size-adjust:100%;-ms-text-size-adjust:100%;-webkit-text-size-adjust:100%;-text-size-adjust:100%;}#container,.content{width:100%;}</style>';
-  }
-?>
+<body>
+<div id="container">
+  <div class="content"></div>
+</div>
+</body>
 <script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
-<script>
-$(document).ready(function(){
-  Sprite = new Image(143,10800);
-  Sprite.src = "img/sprite.png";
-
-	var random = Math.floor(Math.random() * 2) + 1;
-	$(".content").html('<h2 class="title">Online Kingsen</h2><p class="name">Click on the card to start</p><p class="sprite deck-' + random + '"></p><p class="description">Click on the card for a random new card.</p>');
-	$(".content").fadeIn('fast');
-
-	$('.content').on("click", function(){
-		$.get('random.php', function(data){
-			$('.content').html(data).show();
-		});
-	});
-});
-</script>
+<script src="assets/js/main.js"></script>
+<script src="assets/js/ga.js"></script>
+</html>
