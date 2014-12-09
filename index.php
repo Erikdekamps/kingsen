7effbeaf0901b191159ba101e496290b80a2aef8
Erikdekamps <erikdekamps@gmail.com>
2014-12-09 21:19:36 +0100

    Code rewrite 2

--- index.php
+++ index.php
@@ -4,9 +4,9 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=1.0, minimum-scale=1.0, maximum-scale=1.0">
   <meta name="description" content="An online version of the drinking game called Kingsen">
   <meta name="google-site-verification" content="iiV8BbQqGT1TgpJBq5sZjCTmiqj3SbC8zVIraPVp2UM"/>
-  <link rel="stylesheet" href="style.css"/>
-  <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
-  <link rel="icon" href="img/favicon.ico" type="image/x-icon">
+  <link rel="stylesheet" href="assets/css/style.css"/>
+  <link rel="shortcut icon" href="assets/img/favicon.ico" type="image/x-icon">
+  <link rel="icon" href="assets/img/favicon.ico" type="image/x-icon">
   <title>Online Kingsen</title>
 </head>
 <body>
