1bd9fe5842db768b0791d5e04d1ccf036313624e
Erikdekamps <erikdekamps@gmail.com>
2014-12-09 21:22:01 +0100

    File minification

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=1.0, minimum-scale=1.0, maximum-scale=1.0">
   <meta name="description" content="An online version of the drinking game called Kingsen">
   <meta name="google-site-verification" content="iiV8BbQqGT1TgpJBq5sZjCTmiqj3SbC8zVIraPVp2UM"/>
-  <link rel="stylesheet" href="assets/css/style.css"/>
+  <link rel="stylesheet" href="assets/css/style.min.css"/>
   <link rel="shortcut icon" href="assets/img/favicon.ico" type="image/x-icon">
   <link rel="icon" href="assets/img/favicon.ico" type="image/x-icon">
   <title>Online Kingsen</title>
