6ec1bf35786d4766dc9258802fec54f36fb9da27
Erikdekamps <erikdekamps@gmail.com>
2014-12-11 19:29:42 +0100

    Other sharing

--- index.php
+++ index.php
@@ -17,9 +17,7 @@
   <!-- Scripts/Stylesheets -->
   <link rel="stylesheet" href="assets/css/style.min.css"/>
   <script type="text/javascript">var switchTo5x=true;</script>
-  <script type="text/javascript" src="http://w.sharethis.com/button/buttons.js"></script>
   <script type="text/javascript">stLight.options({publisher: "58675fc9-b8df-48e5-b744-74557261f681", doNotHash: false, doNotCopy: false, hashAddressBar: false});</script>
-  <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-542d205c2ffc8507" async="async"></script>
 </head>
 <body>
 <div id="container">
@@ -29,8 +27,9 @@
   <a href="https://api.addthis.com/oexchange/0.8/forward/linkedin/offer?url=http%3A%2F%2Fwww.onlinekingsen.nl&pubid=ra-542d205c2ffc8507&ct=1&title=Online%20Kingsen%20-%20No%20more%20playing%20cards%20needed&pco=tbxnj-1.0" target="_blank"><img src="https://cache.addthiscdn.com/icons/v2/thumbs/32x32/linkedin.png" border="0" alt="LinkedIn"/></a>
   <a href="https://api.addthis.com/oexchange/0.8/forward/google_plusone_share/offer?url=http%3A%2F%2Fwww.onlinekingsen.nl&pubid=ra-542d205c2ffc8507&ct=1&title=Online%20Kingsen%20-%20No%20more%20playing%20cards%20needed&pco=tbxnj-1.0" target="_blank"><img src="https://cache.addthiscdn.com/icons/v2/thumbs/32x32/google_plusone_share.png" border="0" alt="Google+"/></a>
 </div>
-</body>
+<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-542d205c2ffc8507" async="async"></script>
 <script src="http://code.jquery.com/jquery-2.1.0.min.js"></script>
 <script>$(document).ready(function(){Sprite=new Image(143,10800);Sprite.src="../img/sprite.png";var e=Math.floor(Math.random()*2)+1;$(".content").html(""+'<h1 class="title">Online Kingsen</h1>'+'<h2 class="name">Click on the card to start</h2>'+'<p class="sprite deck-'+e+'"></p>'+'<p class="description">Go ahead and share with your friends</p>');$(".content").fadeIn("fast");$(".content").on("click",function(){$.get("random.php",function(e){$(".content").html(e).show()})})})</script>
 <script>(function(e,t,n,r,i,s,o){e["GoogleAnalyticsObject"]=i;e[i]=e[i]||function(){(e[i].q=e[i].q||[]).push(arguments)},e[i].l=1*new Date;s=t.createElement(n),o=t.getElementsByTagName(n)[0];s.async=1;s.src=r;o.parentNode.insertBefore(s,o)})(window,document,"script","//www.google-analytics.com/analytics.js","ga");ga("create","UA-56144415-2","auto");ga("send","pageview")</script>
+</body>
 </html>
