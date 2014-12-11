88b572949c42302663f847f0ac8e0e5183822464
Erik de Kamps <Erik@luciuswebsystems.nl>
2014-12-11 10:30:29 +0100

    Added some more tags

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <!DOCTYPE HTML>
-<html http-equiv="Content-Type" content="text/html; charset=utf-8">
+<html http-equiv="Content-Type" content="text/html; charset=utf-8" lang="en">
 <head>
   <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=1.0, minimum-scale=1.0, maximum-scale=1.0">
-  <meta name="description" content="An online version of the drinking game called Kingsen">
+  <meta name="description" content="An online version of the drinking game called Kingsen. Go ahead and give it a try with your friends!">
   <meta name="google-site-verification" content="iiV8BbQqGT1TgpJBq5sZjCTmiqj3SbC8zVIraPVp2UM"/>
-
+  <link rel="publisher" href="https://plus.google.com/+ErikdeKamps" />
   <link rel="stylesheet" href="assets/css/style.min.css"/>
   <link rel="shortcut icon" href="assets/img/favicon.ico" type="image/x-icon">
   <link rel="icon" href="assets/img/favicon.ico" type="image/x-icon">
